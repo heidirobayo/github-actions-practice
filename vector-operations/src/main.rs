4fe282dccf7ee1b6ae41ac7436662099d893c54c
heidirobayo <150488054+heidirobayo@users.noreply.github.com>
2023-12-12 11:47:24 +0000

    Add some vector operations

--- vector-operations/src/main.rs
+++ vector-operations/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    // instantiate  a  new vector
+    let mut vec = Vec::new();
+    // push elements  into  the  vector
+    vec.push(1);
+    vec.push(2);
+    vec.push(3);
+
+    
+    // iterate the vector with for loop
+    for x in &vec {
+        println!("{}", x);
+    }
+
+    // iterate the vector with while loop
+    while let Some(x) = vec.pop() {
+        println!("{}", x);
+    }
 }
