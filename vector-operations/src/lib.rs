e22d5b4e309b61f822ed745c2151b4da79138058
heidirobayo <150488054+heidirobayo@users.noreply.github.com>
2023-12-12 14:57:36 +0000

    add get_slices function in lib file and test in main

--- vector-operations/src/lib.rs
+++ vector-operations/src/lib.rs
@@ -18,3 +18,15 @@ pub fn random_numbers_vector() -> Vec<i32> {
     }
     vec
 }
+
+// create a function to get slices of a vector
+pub fn get_slices(vec: Vec<i32>) -> Vec<Vec<i32>> {
+    let mut vec_of_vec = Vec::new();
+    for i in 0..vec.len() {
+        if i <= vec.len() - 2 {
+            println!("{} {} {}", i, vec[i], vec[i + 1]);
+            vec_of_vec.push(vec[i..i + 2].to_vec());
+        }
+    }
+    vec_of_vec
+}
--- vector-operations/src/main.rs
+++ vector-operations/src/main.rs
@@ -26,4 +26,8 @@ fn main() {
     println!("Get the vector of random numbers: \n");
     let vec = vector_operations::random_numbers_vector();
     println!("The vector of random numbers is => {:?}", vec);
+
+    println!("Get slices from a vector: \n");
+    let vec = vector_operations::get_slices(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    println!("The slices of the vector are => {:?}", vec);
 }
